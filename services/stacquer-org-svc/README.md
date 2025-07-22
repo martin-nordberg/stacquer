8acd7051b0c2f1d150569e987ba6b87c4d732d9e
martin-nordberg <martinnordberg3@gmail.com>
2025-07-22 08:35:32 -0400

    Created skeleton Stacquer Studio front end and back end using bun, SolidJS, and Hono.

--- services/stacquer-org-svc/README.md
+++
@@ -1,2 +0,0 @@
-
-# Stacquer Organization Editing Service
--- shared/stacquer-shared/vitest/example.test.ts
+++
@@ -1,7 +0,0 @@
-import {describe, expect, test} from 'vitest'
-
-describe('Examples', () => {
-    test('Example', () => {
-        expect(1 + 1).toBe(2)
-    })
-})
